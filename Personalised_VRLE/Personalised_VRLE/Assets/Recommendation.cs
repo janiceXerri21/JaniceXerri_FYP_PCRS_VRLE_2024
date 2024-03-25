c9d1c41ee1d6d2aac0da6fec1ff5f5fdab73ae7d
janiceXerri21 <103377434+janiceXerri21@users.noreply.github.com>
2024-03-25 16:57:25 +0100

    Creating the variables for the fields of the CSV file

--- Personalised_VRLE/Personalised_VRLE/Assets/Recommendation.cs
+++ Personalised_VRLE/Personalised_VRLE/Assets/Recommendation.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Recommendation : MonoBehaviour
+public class Recommendation //purely informational
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int CourseCode;
+    public string Degree;
+    public string DegreeSpecializations;
+    public string Campus;
+    public string KeySkills;
+    public string Filter;
+    public float Success_rate;
 }
+
+
