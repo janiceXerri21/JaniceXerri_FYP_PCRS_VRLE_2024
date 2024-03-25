8668ff319a2416914d07ffd1238e316f3e6e75cc
janiceXerri21 <103377434+janiceXerri21@users.noreply.github.com>
2024-03-25 17:02:03 +0100

    Capable of reading and parsing the data in a csv file in Unity whilst also displaying that data in Debug Console
     (to run the script (File Reader)  it is connected to the blackboard object)

--- Personalised_VRLE/Personalised_VRLE/Assets/FileReader.cs
+++ Personalised_VRLE/Personalised_VRLE/Assets/FileReader.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class FileReader : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //to store the information 
+    List<Recommendation> recommendations = new List<Recommendation>();
+    //using this for initialisation
     void Start()
     {
-        
+        TextAsset recommendationstext = Resources.Load<TextAsset>("user_1001_course_recommendations");
+
+        //text assest class enabling us to access the data in a string
+        //creating an array - splitting based on the new line
+        string[] data = recommendationstext.text.Split(new char[] { '\n' });
+
+        //Debug.Log(data.Length); - was used to check if it reads the extra line in the csv file 
+
+        //reading and parsing each line individually (note avoiding the first (of the fields) and the last line)
+        for (int i = 1; i < data.Length - 1; i++)
+        {
+            // Using regular expressions to split the row by commas that are not within quotes.
+            string[] row = System.Text.RegularExpressions.Regex.Split(data[i],
+                                ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+            // Trim each element to remove unwanted white spaces
+            for (int j = 0; j < row.Length - 1; j++)
+            {
+                row[j] = row[j].Trim();
+            }
+
+            Recommendation r = new Recommendation();
+            int.TryParse(row[0], out r.CourseCode);
+            r.Degree = row[1].Replace("\"", ""); // Remove quotes
+            r.DegreeSpecializations = row[2].Replace("\"", "");
+            r.Campus = row[3].Replace("\"", "");
+            r.KeySkills = row[4].Replace("\"", ""); // Remove quotes from the skills
+            r.Filter = row[5].Replace("\"", "");
+
+            float.TryParse(row[6].Replace("%", "").Replace("\"", ""), out r.Success_rate); //to convert the string to float, if there is data
+
+            //adding the recommendation to the list
+            recommendations.Add(r);
+        }
+
+        //to see if it is working
+        foreach (Recommendation r in recommendations)
+        {
+            Debug.Log(r.CourseCode + ", " + r.Degree + ", " + r.DegreeSpecializations + ", " + r.Campus + ", " + r.KeySkills + ", " + r.Filter + ", " + r.Success_rate);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
