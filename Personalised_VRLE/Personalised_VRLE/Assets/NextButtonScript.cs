f971535ab13d51fba78d071cbc948e2b154b9bf3
janiceXerri21 <103377434+janiceXerri21@users.noreply.github.com>
2024-04-01 22:22:08 +0200

    Both previous and next course button display the appropriate data.

---
+++ Personalised_VRLE/Personalised_VRLE/Assets/NextButtonScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextButtonScript : MonoBehaviour
+{
+    public GameObject[] otherButtons;
+    public UIController uiController; // Assign this in the Inspector
+
+    private void Awake()
+    {
+        // Find and assign the UIController if not set in the Inspector
+        if (uiController == null)
+        {
+            uiController = FindObjectOfType<UIController>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HandPositionApproximation"))
+        {
+            // Change this button's colour to indicate it's pressed
+            Renderer renderer = GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material.color = Color.green;
+            }
+
+            // Disable the other buttons
+            foreach (GameObject button in otherButtons)
+            {
+                button.GetComponent<BoxCollider>().enabled = false;
+            }
+
+            // Call the NextCourse method from the UIController
+            uiController.NextCourse();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("HandPositionApproximation"))
+        {
+            // Change this button's colour back to its default state
+            Renderer renderer = GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material.color = Color.white;
+            }
+
+            // Re-enable the other buttons
+            foreach (GameObject button in otherButtons)
+            {
+                button.GetComponent<BoxCollider>().enabled = true;
+            }
+        }
+    }
+}
---
+++ Personalised_VRLE/Personalised_VRLE/Assets/PreviousButtonScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviousButtonScript : MonoBehaviour
+{
+    public GameObject[] otherButtons;
+    public UIController uiController; // Assign this in the Inspector
+
+    private void Awake()
+    {
+        // Find and assign the UIController if not set in the Inspector
+        if (uiController == null)
+        {
+            uiController = FindObjectOfType<UIController>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HandPositionApproximation"))
+        {
+            // Change this button's colour to green
+            Renderer renderer = GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material.color = Color.green;
+            }
+
+            // Disable the other buttons so only one can be pressed at a time
+            foreach (GameObject button in otherButtons)
+            {
+                button.GetComponent<BoxCollider>().enabled = false;
+            }
+
+            // Call the PreviousCourse method from the UIController
+            uiController.PreviousCourse();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("HandPositionApproximation"))
+        {
+            // Change this button's colour back to white
+            Renderer renderer = GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material.color = Color.white;
+            }
+
+            // Re-enable all the buttons
+            foreach (GameObject button in otherButtons)
+            {
+                button.GetComponent<BoxCollider>().enabled = true;
+            }
+        }
+    }
+}
